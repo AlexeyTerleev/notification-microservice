50e73d02a7f9d089320751b215ebac573c0dea11
alexey_t <alexterleev682004@gmail.com>
2024-03-04 17:37:02 +0300

    refactor

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
+import logging
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO, 
+        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+    )
+
+    
 class RabbitMQSettings(BaseModel):
     RABBIT_USER: str
     RABBIT_PASS: str
@@ -11,14 +19,37 @@ class RabbitMQSettings(BaseModel):
     RESET_PASSWORD_QUEUE: str = "reset-password-stream"
     DEAD_LETTER_QUEUE: str = "dead-letter"
 
-    def get_url(self):
+    @property
+    def url(self):
         return f"amqp://{self.RABBIT_USER}:{self.RABBIT_PASS}@{self.RABBIT_HOST}:{self.RABBIT_PORT}/"
+    
+
+class MongoDBSettings(BaseModel):
+    MONGO_USER: str
+    MONGO_PASS: str
+    MONGO_HOST: str
+    MONGO_PORT: str
+    MONGO_AUTH_SRC: str
+
+    def get_url(self):
+        return f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/?authSource={settings.mongo_db.MONGO_AUTH_SRC}"
+
+# "mongodb://admin:password@notification-mongodb:27017/?authSource=admin"
+# rs.initiate({_id:'rs0',members:[{_id:0,host:'notification-mongodb:27017'}]})
+    
+class AWS(BaseModel):
+    AWS_URL: str
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_REGION_NAME: str
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     
     rabbit_mq: RabbitMQSettings
+    mongo_db: MongoDBSettings
+    aws: AWS
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,60 +1,19 @@
 import logging
-import aio_pika
 import asyncio
-import bson
 
-from app.config import settings
-
-logging.basicConfig(
-    level=logging.INFO,  # Set the log level to INFO
-    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
-)
-
-
-async def on_message(message) -> None:
-    try:
-        await process_message(message)
-        raise TypeError("test")
-        await message.ack()
-    except Exception as e:
-        delivery_count = message.headers.get('x-delivery-count', 0)
-        if delivery_count < 5:
-            logging.warning(
-                f"Message (id = {message.message_id}) raise exeption (attemp {delivery_count})."
-            )
-            await message.reject(requeue=True)
-        else:
-            logging.error(
-                f"Message (id = {message.message_id}) moved to dead letter queue."
-            )
-            await move_to_dead_letter_queue(message)
-            await message.reject()
-
-async def process_message(message) -> None:
-    logging.info(f"Received message body: {bson.decode(message.body)}, headers: {message.headers}")
-
-
-async def move_to_dead_letter_queue(message):
-    connection = await aio_pika.connect(settings.rabbit_mq.get_url())
-    async with connection:
-        channel = await connection.channel()
-        dead_letter_queue = await channel.declare_queue(settings.rabbit_mq.DEAD_LETTER_QUEUE, durable=True)
-        await channel.default_exchange.publish(message, routing_key=dead_letter_queue.name)
-
+from app.config import setup_logging
+from app.constants import APP_RUNNING
+from services.consume_service import ConsumeService
 
 
 async def main():
-    connection = await aio_pika.connect_robust(settings.rabbit_mq.get_url())
-
-    async with connection:
-        channel = await connection.channel()
-        await channel.set_qos(prefetch_count=1)
-        queue = await channel.declare_queue(settings.rabbit_mq.RESET_PASSWORD_QUEUE, durable=True, arguments={"x-queue-type": "quorum"})
-        await queue.consume(on_message)
-
-        await asyncio.Future()
+    consume_service = ConsumeService()
+    await consume_service.start_consume()
 
 
 if __name__ == "__main__": 
-    logging.info("running")
+
+    setup_logging()
+
+    logging.info(APP_RUNNING)
     asyncio.run(main())
